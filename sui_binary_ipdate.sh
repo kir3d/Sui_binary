c48a7432fe9d1a031220b8467c017b679fb96eec
kir3d <kir3d@mail.ru>
2022-08-03 19:48:39 +0600

    Delete sui_binary_ipdate.sh

--- sui_binary_ipdate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo systemctl stop suid
-rm -rf $HOME/suidb $HOME/genesis.blob
-wget  -qO- $(wget -qO-  https://api.github.com/repos/kir3d/Sui_binary/releases/latest | grep browser_download_url | awk '{print $2}' | sed 's/"//g')| tar -C /usr/bin/ -xzf -
-wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-sudo systemctl start suid
