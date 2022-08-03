9e90579690ca3f2d7495792d61580d7408de0ce7
kir3d <kir3d@mail.ru>
2022-08-04 03:43:54 +0600

    Update sui_binary.sh

--- sui_binary.sh
+++ sui_binary.sh
@@ -18,7 +18,7 @@ mkdir -p $HOME/.sui
 cd $HOME/.sui
 
 echo Dowloading binary
-wget  -qO- $(wget -qO-  https://api.github.com/repos/kir3d/Sui_binary/releases/latest | grep browser_download_url | awk '{print $2}' | sed 's/"//g')| tar -C /usr/bin/ -xzf -
+wget  -qO- $(wget -qO-  https://api.github.com/repos/kir3d/Sui_binary/releases/latest | grep browser_download_url | awk '{print $2}' | sed 's/"//g')| tar -C /usr/local/bin/ -xzf -
 wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 
 printf "# Update this value to the location you want Sui to store its database
