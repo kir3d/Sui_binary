414de7f08ae29c431315027dd0dab5cf4b24f980
kir3d <kir3d@mail.ru>
2022-08-03 23:10:25 +0600

    Update sui_binary_update.sh

--- sui_binary_update.sh
+++ sui_binary_update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
- sudo systemctl stop suid
- rm -rf $HOME/suidb $HOME/genesis.blob
- wget  -qO- $(wget -qO-  https://api.github.com/repos/kir3d/Sui_binary/releases/latest | grep browser_download_url | awk '{print $2}' | sed 's/"//g')| tar -C /usr/bin/ -xzf -
- wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
- sudo systemctl start suid
+echo Stop suid.service
+sudo systemctl stop suid
+echo Deleteting DB and Genesis
+rm -rf $HOME/suidb $HOME/genesis.blob
+echo Downloadding binary
+wget  -qO- $(wget -qO-  https://api.github.com/repos/kir3d/Sui_binary/releases/latest | grep browser_download_url | awk '{print $2}' | sed 's/"//g')| tar -C /usr/bin/ -xzf -
+echo Downloadding Genesis
+wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+echo Start suid.service
+sudo systemctl start suid
