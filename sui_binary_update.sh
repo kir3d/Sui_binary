14db74b6dec2641642d0d6f59fd555a42c05fc4e
kir3d <kir3d@mail.ru>
2022-08-03 23:22:34 +0600

    Update sui_binary_update.sh

--- sui_binary_update.sh
+++ sui_binary_update.sh
@@ -9,3 +9,10 @@ echo Downloadding Genesis
 wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 echo Start suid.service
 sudo systemctl start suid
+
+echo Checking node
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n sui_log -v "sudo journalctl -fn 100 -u suid" -a
+sleep 0.5
+wget -qO-  -t 1 -T 5 --header 'Content-Type: application/json' --post-data '{ "jsonrpc":"2.0", "id":1, "method":"sui_getRecentTransactions", "params":[5] }' "http://127.0.0.1:9000/" | jq
+cd
+echo For check log type $(tput setaf 2)sui_log$(tput setaf 15) 
