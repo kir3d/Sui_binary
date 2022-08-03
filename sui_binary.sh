77c6cc7212907d3d292358aefc2559c8b1a68ada
kir3d <kir3d@mail.ru>
2022-08-04 04:31:48 +0600

    Update sui_binary.sh

--- sui_binary.sh
+++ sui_binary.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo $(tput setaf 15)Starting
+echo -e "\033[0;0mStarting"
 if [ $(dpkg-query -W -f='${Status}' jq 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  echo Install JQ
+  echo -e "Install JQ"
   apt update
   apt-get install jq curl -y ;
 fi
@@ -11,14 +11,14 @@ if test -f "/etc/systemd/system/suid.service"
 then
   echo 
 else
-  echo Stop service
+  echo -e "Stop service"
   sudo systemctl stop suid
 fi
 
 mkdir -p $HOME/.sui
 cd $HOME/.sui
 
-echo Dowloading binary
+echo -e "Dowloading binary"
 wget  -qO- $(wget -qO-  https://api.github.com/repos/kir3d/Sui_binary/releases/latest | grep browser_download_url | awk '{print $2}' | sed 's/"//g')| tar -C /usr/local/bin/ -xzf -
 wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 
@@ -31,14 +31,14 @@ genesis:
   # Update this to the location of where the genesis file is stored
   genesis-file-location: \"/root/.sui/genesis.blob\"" > $HOME/.sui/fullnode.yaml
 
-echo Firewall open ports: 8080, 9000, 9184
+echo -e "Firewall open ports: 8080, 9000, 9184"
 ufw allow ssh > /dev/null 2>&1
 ufw allow 8080 > /dev/null 2>&1
 ufw allow 9000 > /dev/null 2>&1
 ufw allow 9184 > /dev/null 2>&1
 echo y | ufw enable > /dev/null 2>&1
 
-echo Creating suid.service
+echo -e "Creating suid.service"
 printf "[Unit]
 Description=Sui node
 After=network-online.target
@@ -56,20 +56,20 @@ sudo systemctl enable suid
 sudo systemctl restart suid
 sudo journalctl --vacuum-size=1G > /dev/null 2>&1
 
-echo Address and keys
+echo -e "Address and keys"
 echo "http://$(curl -s ifconfig.me):9000/" > $HOME/.sui/address.txt
-echo Copy$(tput setaf 2) $(cat address.txt) $(tput setaf 15)and paste to Discord. 
-echo Right click and Open link:$(tput setaf 2) https://discord.com/channels/916379725201563759/986662676073709568 $(tput setaf 15)
+echo -e "Copy \033[0;32m$(cat address.txt) \033[0;0mand paste to Discord. 
+echo Right click and Open link:$(tput setaf 2) https://discord.com/channels/916379725201563759/986662676073709568"
 echo y | sui client > /dev/null 2>&1
-echo Backup this
+echo -e "\033[0;32mBackup this"
 sui keytool list > $HOME/.sui/keys.txt
 cat ~/.sui/keys.txt
 
-echo Checking node
+echo -e "Checking node"
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n sui_log -v "sudo journalctl -fn 100 -u suid" -a
 sleep 0.5
 wget -qO-  -t 1 -T 5 --header 'Content-Type: application/json' --post-data '{ "jsonrpc":"2.0", "id":1, "method":"sui_getRecentTransactions", "params":[5] }' "http://127.0.0.1:9000/" | jq
 cd
-echo echo $(tput setaf 15)StartingFor check log type $(tput setaf 2)sui_log$(tput setaf 15)
-echo echo $(tput setaf 15)Done
+echo -e "\033[0;0m)StartingFor check log type \033[0;32msui_log"
+echo -e "\033[0;32mDone"
 
