5543db75b36ae551f5380a7ecf846c6f7237a21f
kir3d <kir3d@mail.ru>
2023-01-05 12:20:12 +0600

    Update sui_binary.sh
    change url binary to official github release

--- sui_binary.sh
+++ sui_binary.sh
@@ -19,8 +19,10 @@ mkdir -p $HOME/.sui
 cd $HOME/.sui
 
 echo -e "Dowloading binary"
-wget  -qO- $(wget -qO-  https://api.github.com/repos/kir3d/Sui_binary/releases/latest | grep browser_download_url | awk '{print $2}' | sed 's/"//g')| tar -C /usr/local/bin/ -xzf -
+for i in $( wget -qO-  https://api.github.com/repos/MystenLabs/sui/releases/latest | grep browser_download_url | awk '{print $2}' | sed 's/"//g'); do wget $i; done; 
+for i in *; do chmod +x $i; mv $i /usr/local/bin//$i; done
 wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+
 
 printf "# Update this value to the location you want Sui to store its database
 db-path: \"/root/.sui/db\"
